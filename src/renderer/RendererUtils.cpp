a013b655a1125a1fb52087ac7fc70f67bf619f3e
Emily Winnink <emily.winnink@gmail.com>
2026-03-10 17:09:21 +0100

    Created utils file

---
+++ src/renderer/RendererUtils.cpp
@@ -0,0 +1,47 @@
+/*
+Created by: Emily (Em_iIy) Winnink
+Created on: 10/03/2026
+*/
+
+#include "Renderer.hpp"
+
+void	Renderer::togglePause()
+{
+	_pause = !_pause;
+}
+
+void	Renderer::setLightingMode(int mode)
+{
+	_lightingMode = mode;
+}
+
+void	Renderer::swapFrameBuffer(int direction)
+{
+	currentFrameBufferIdx = (currentFrameBufferIdx + frameBufferIds.size() + direction) % frameBufferIds.size();
+	std::cout << "Swapped framebuffer to <" << frameBufferIds[currentFrameBufferIdx].first << ">: " << frameBufferIds[currentFrameBufferIdx].second << std::endl;
+}
+
+mlm::mat4	&Renderer::getProjection()
+{
+	return (_projection);
+}
+
+mlm::mat4	&Renderer::getView()
+{
+	return (_view);
+}
+
+mlm::mat4	&Renderer::getLightProjection()
+{
+	return (_lightProjection);
+}
+
+mlm::mat4	&Renderer::getLightView()
+{
+	return (_lightView);
+}
+
+mlm::vec3	&Renderer::getSunPos()
+{
+	return (_sunPos);
+}
